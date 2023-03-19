c7ed1b46e663753ac531a2bd5fdfe60d6ec7cec1
David Barraud <davidbarraud@gmail.com>
2023-03-19 17:00:45 -0300

    cambios

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// código para escribir en el Placeholder
+
+let nombre = prompt("Ingrese su nombre:");
+
+let myDom = document;
+let myApp = document.getElementById("myApp");
+myApp.innerHTML="Bienvenido ${nombre}";
